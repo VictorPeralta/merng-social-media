bc3f9e9c21429958d53bac8992905b3747bb04cc
Victor Peralta <peralta.r.victor@gmail.com>
2020-12-12 13:37:46 -0800

    Finished backend

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 const postResolvers = require('./posts');
 const userResolvers = require('./users')
+const commentResolvers = require('./comments');
 
 module.exports = {
     Query: {
@@ -8,6 +9,7 @@ module.exports = {
     Mutation:
     {
         ...userResolvers.Mutation,
-        ...postResolvers.Mutation
+        ...postResolvers.Mutation,
+        ...commentResolvers.Mutation
     }
 };
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/checkAuth');
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 module.exports = {
     Query: {
@@ -23,7 +23,7 @@ module.exports = {
                     throw new Error('Post not found');
                 }
             } catch (error) {
-                throw new Error(error);
+                throw error;
             }
         }
     },
@@ -50,6 +50,28 @@ module.exports = {
                     return 'Post deleted sucessfully';
                 }else{
                     throw new AuthenticationError('Action not allowed');
+                }
+            }catch(err){
+                throw new Error(err)
+            }
+        },
+        async likePost(_, {postId}, ctx){
+            const {username} = checkAuth(ctx);
+            try{
+                const post = await Post.findById(postId);
+                if(post){
+                    if(post.likes.find(like => like.username === username)){
+                        post.likes = post.likes.filter(like => like.username !== username);
+                    }else{
+                        post.likes.push({
+                            username,
+                            createdAt: new Date().toISOString()    
+                        });
+                    }
+                    await post.save();
+                    return post;
+                }else{
+                    throw new UserInputError('Post not found')
                 }
             }catch(err){
                 throw new Error(err)
